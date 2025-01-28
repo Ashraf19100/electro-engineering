107b15e88a0b3e00a6990ef48ee8ce2bdbaf9064
Ashraf19100 <nafizashraaf@gmail.com>
2025-01-28 16:05:49 +0600

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function ($) {
+	//meanmenu
+	$("#navbar nav").meanmenu();
+	  
+	  $(".slider-area").owlCarousel({
+		autoWidth: false,
+		items: 1,
+		loop: true,
+		nav: false,
+		dots: true,
+		autoplay: true,
+		responsive: {
+		  0: {
+			items: 1,
+		  },
+		  767: {
+			items: 1,
+		  },
+		  1024: {
+			items: 1,
+		  },
+		},
+	  });
+	  $(".brand-area").owlCarousel({
+		autoWidth: false,
+		items: 5,
+		loop: true,
+		nav: true,
+		dots: false,
+		autoplay: true,
+		autoplayTimeout:1200,
+		navText : ["<i class='fa fa-arrow-left'></i>","<i class='fa fa-arrow-right'></i>"],
+		mouseDrag: true,
+		responsive: {
+		  0: {
+			items: 1,
+		  },
+		  767: {
+			items: 3,
+		  },
+		  1024: {
+			items: 8,
+		  },
+		},
+	  });
+	//jQuery Sticky Area
+	  $(".sticky-area").sticky({
+		topSpacing: 0,
+	  });
+      
+});
